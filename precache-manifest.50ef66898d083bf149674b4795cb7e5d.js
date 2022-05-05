756f149feef23b81b5fd8baa8a626457e40ea958
VidushiSingla99 <vidushisingla99@gmail.com>
2022-05-05 08:47:45 +0530

    Updates

---
+++ precache-manifest.50ef66898d083bf149674b4795cb7e5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2111ffc95bd0b447ea7f",
+    "url": "/front-end/static/css/main.af5107c2.chunk.css"
+  },
+  {
+    "revision": "2111ffc95bd0b447ea7f",
+    "url": "/front-end/static/js/main.2111ffc9.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/front-end/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "5823ae874eb2263d6e5e",
+    "url": "/front-end/static/js/2.5823ae87.chunk.js"
+  },
+  {
+    "revision": "0521f3b6a7980f404f4d",
+    "url": "/front-end/static/js/runtime~main.0521f3b6.js"
+  },
+  {
+    "revision": "f42dcb86ec0a29b4e41cb1f518258083",
+    "url": "/front-end/index.html"
+  }
+];
